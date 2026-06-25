b428e01b2c26951224b6e00eb1f261a2edaebedd
Brajeshnaik-gif <brnaik63@gmail.com>
2026-06-25 15:08:31 +0530

    initial-push

---
+++ Selenium-hybdrid/src/project/project/SampleTest.java
@@ -0,0 +1,28 @@
+package project.project;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
+public class SampleTest {
+	
+   ChromeDriver driver;
+  @Test
+  public void Noukari() throws InterruptedException {
+	  //driver.get("https://www.techlearn.in/admin");
+	  driver.get("https://www.naukri.com/");
+	  Thread.sleep(3000);
+	  driver.findElement(By.id("login_Layer")).click();
+	  Thread.sleep(3000);
+	  driver.findElement(By.xpath("//*[@id=\"root\"]/div[4]/div[2]/div/div/div[2]/div/form/div[2]/input")).sendKeys("rajesh");
+	  Thread.sleep(3000);
+	 // driver.findElement(By.id("user_login")).sendKeys("rajesh");
+  }
+  @BeforeTest
+  public void beforeTest() {
+	   driver = new ChromeDriver();
+	   
+  }
+
+}
